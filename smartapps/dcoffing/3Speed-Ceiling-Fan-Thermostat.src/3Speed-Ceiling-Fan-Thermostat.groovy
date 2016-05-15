c1930cb898a612805128b117f58d182c6600277e
Dale Coffing <dcoffing@gmail.com>
2016-05-15 05:40:49 -0600

    Update 3Speed-Ceiling-Fan-Thermostat.groovy

--- smartapps/dcoffing/3Speed-Ceiling-Fan-Thermostat.src/3Speed-Ceiling-Fan-Thermostat.groovy
+++ smartapps/dcoffing/3Speed-Ceiling-Fan-Thermostat.src/3Speed-Ceiling-Fan-Thermostat.groovy
@@ -27,6 +27,7 @@
  *
  *   * Change Log
  * 2016-5-15 fixed fan differenial decimal point error; removed range: "1..99", removed all fanDimmer.setLevel(0)
+ * 	     added high resolution iconX3Url
  * 2016-5-14 Fan temperature differential variable added, change sensor to tempSensor,
  * 2016-5-13 (g)replace ELSE IF for SWITCH statements on fan speeds, removed emergency temp control
  * 2016-5-12 added new icons for 3SFC, colored text in 3SFC125x125.png and 3sfc250x250.png
@@ -49,6 +50,7 @@ definition(
     category: "My Apps",
 	iconUrl: "https://raw.githubusercontent.com/dcoffing/SmartThingsPublic/master/smartapps/dcoffing/3Speed-Ceiling-Fan-Thermostat.src/3scft125x125.png", 
    	iconX2Url: "https://raw.githubusercontent.com/dcoffing/SmartThingsPublic/master/smartapps/dcoffing/3Speed-Ceiling-Fan-Thermostat.src/3scft250x250.png",
+	iconX3Url: "https://raw.githubusercontent.com/dcoffing/SmartThingsPublic/master/smartapps/dcoffing/3Speed-Ceiling-Fan-Thermostat.src/3scft250x250.png",
 )
 
 preferences {
